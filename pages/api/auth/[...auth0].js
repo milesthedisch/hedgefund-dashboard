5d5ff410bb4c0209ce94485404932fbb124aabb6
milesthedisch <milesthedisch@gmail.com>
2022-03-01 11:41:34 +1100

    Revert dangerous

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -1,12 +1,9 @@
 import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
 
-export default (req, res) => {
-  res.send(JSON.stringify(process.env));
-};
-// export default handleAuth({
-//   async login(req, res) {
-//     await handleLogin(req, res, {
-//       returnTo: "/dashboard",
-//     });
-//   },
-// });
+export default handleAuth({
+  async login(req, res) {
+    await handleLogin(req, res, {
+      returnTo: "/dashboard",
+    });
+  },
+});
