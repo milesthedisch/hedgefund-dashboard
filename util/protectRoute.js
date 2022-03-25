5e12888062d870e482bff1c8ef987bc64f56fc7d
milesthedisch <milesthedisch@gmail.com>
2022-03-25 16:34:13 +1100

    Update protectRoute for debuggin

--- util/protectRoute.js
+++ util/protectRoute.js
@@ -1,5 +1,7 @@
 export default function protectRoute(routeHandler) {
   return function wrappedRoute(req, res) {
+    console.log(req.body);
+
     if (req.body.secret !== process.env.AUTH0_HOOK_SECRET) {
       res.status(401).json({
         error: "not_authenticated",
