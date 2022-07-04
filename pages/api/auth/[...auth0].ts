60574150c6f38d73f6d29a62705f739b6669b65c
milesthedisch <milesthedisch@gmail.com>
2022-07-03 23:01:51 -0400

    Updated auth to redirect to index to decide what page to land on.

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -1,20 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { handleAuth, handleLogin, handleLogout } from "@auth0/nextjs-auth0";
-import { getSession } from "@auth0/nextjs-auth0";
+import {
+  handleCallback,
+  handleAuth,
+  handleLogin,
+  handleLogout,
+} from "@auth0/nextjs-auth0";
 
 export default handleAuth({
   async login(req: NextApiRequest, res: NextApiResponse) {
-    const { user } = getSession(req, res);
-    const isAudit =
-      user["https://balmoral-dashboard.vercel.com/roles"].includes("audit");
-    const isAdmin =
-      user["https://balmoral-dashboard.vercel.com/roles"].includes("admin");
-
-    const adminStart = "/admin/strategies";
-    const auditStart = "/admin/audit";
-
     await handleLogin(req, res, {
-      returnTo: isAdmin ? adminStart : isAudit ? auditStart : "/dashboard",
+      returnTo: "/",
     });
   },
   async logout(req: NextApiRequest, res: NextApiResponse) {
