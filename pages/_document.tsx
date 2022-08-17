d5addb94b88f1d00d6bbc01bafe307ac44caa24f
milesthedisch <milesthedisch@gmail.com>
2022-08-17 14:20:08 -0600

    Update npm and update mui server render css

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,6 +19,7 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          {(this.props as any).emotionStyleTags}
         </Head>
         <body>
           <Main />
